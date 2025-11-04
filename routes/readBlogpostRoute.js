f3f62bbee5e94842102556b112bcba5153141a2d
timtim40a <timtim40a@gmail.com>
2025-11-04 21:24:10 +0000

    READ now shows full authors' data

--- routes/readBlogpostRoute.js
+++ routes/readBlogpostRoute.js
@@ -2,16 +2,47 @@ import { db } from '../db/db.js'
 
 export const readBlogpostRoute = (req, res) => {
     const blogpostId = parseInt(req.params.id)
-    db.query(
-        `SELECT b.*, GROUP_CONCAT(CONCAT(a.name, " ", a.surname) SEPARATOR ", ") AS authors FROM blogposts AS b JOIN blogposts_authors as j ON b.id = j.blogpost_id JOIN authors AS a ON j.author_id = a.id WHERE b.id = ${blogpostId} GROUP BY b.id`,
-        (err, results) => {
-            if (err) {
-                res.status(500).json({
-                    status: '500',
-                    message: 'there is a problem loading the database X(',
-                })
-            }
-            res.status(200).json(results)
+
+    const sql = `
+        SELECT b.*, 
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'id', a.id,
+                'name', a.name,
+                'surname', a.surname,
+                'email', a.email
+            )
+        ) AS authors 
+        FROM blogposts AS b 
+        JOIN blogposts_authors as j 
+            ON b.id = j.blogpost_id 
+        JOIN authors AS a 
+            ON j.author_id = a.id 
+        WHERE b.id = ? 
+        GROUP BY b.id`
+
+    db.query(sql, [blogpostId], (err, results) => {
+        if (err) {
+            res.status(500).json({
+                status: '500',
+                message: 'there is a problem loading the database X(',
+            })
         }
-    )
+
+        if (results.length === 0) {
+            res.status(404).json({
+                status: '404',
+                message: 'not found :?',
+            })
+        }
+
+        const blogpost = results[0]
+        try {
+            blogpost.authors = JSON.parse(blogpost.authors)
+        } catch {
+            // if MySQL already returns as object, skip
+        }
+
+        res.status(200).json(results)
+    })
 }
